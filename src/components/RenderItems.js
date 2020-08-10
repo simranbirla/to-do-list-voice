27a6f0bb2f7e2ecef441cb62d8a27faddf3565cd
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-10 22:32:24 +0530

    delete

--- src/components/RenderItems.js
+++ src/components/RenderItems.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import "./SearchBox.css";
 
 class RenderItems extends Component {
+  deleteItem = (index) => {
+    console.log(index);
+  };
+
   renderList = () => {
     return this.props.thingsList.map((thing, index) => {
       return (
         <div className="thing-container" key={index}>
           <p className="thing">
             <span className="span">{index + 1}.</span> {thing}
+            <button onClick={() => this.deleteItem(index)}>X</button>
           </p>
         </div>
       );
